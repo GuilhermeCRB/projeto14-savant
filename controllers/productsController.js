8b5879bfd21eb40b32a81738ad13080223e25503
Jorge Tadeu <jsbatalha@gmail.com>
2022-05-13 18:45:06 -0300

    Fixing bugs

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,21 +3,21 @@ import db from "../db.js";
 export async function getProducts(req, res){
     const title = req.query.title;
     const gener = req.query.gener;
-    
+
     try{
         
         if(!title && !gener){
-            const products = await db.collection('products').find();
+            const products = await db.collection('products').find().toArray();
 
             res.status(200).send(products);
         }
 
         if(title){
-            const product = await db.collection('products').find({title});
+            const product = await db.collection('products').find({ title: title.replaceAll("-", " ")}).toArray();
             res.status(200).send(product);
         }
         if(gener){
-            const products = await db.collection('products').findAll({gener})
+            const products = await db.collection('products').findAll({gener}).toArray();
 
             res.status(200).send(products);
         }
