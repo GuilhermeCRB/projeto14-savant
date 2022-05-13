4be6f3a328858761bd78e25de42c96501277b15c
Jorge Tadeu <jsbatalha@gmail.com>
2022-05-13 18:31:15 -0300

    Get products

---
+++ controllers/productsController.js
@@ -0,0 +1,28 @@
+import db from "../db.js";
+
+export async function getProducts(req, res){
+    try{
+        const title = req.query.title;
+        const gener = req.query.gener;
+        
+        if(!title && !gener){
+            const products = await db.collection('products').find();
+
+            res.status(200).send(products);
+        }
+
+        if(title){
+            const product = await db.collection('products').find({title});
+            res.status(200).send(product);
+        }
+        if(gener){
+            const products = await db.collection('products').findAll({gener})
+
+            res.status(200).send(products);
+        }
+    }
+    catch(err){
+        console.log(chalk.red.bold(`\nWARNING: sign up failed! \nError: \n`), e);
+        res.status(500).send(e);
+    }
+}
