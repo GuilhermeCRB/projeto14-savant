d162a075fde834a63f42e5ea31bdc7ffab5e16cb
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-05-14 19:01:04 -0300

    fix: changes wrong function findAll to find on products controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,7 @@
 import db from "../db.js";
 
+import chalk from "chalk";
+
 export async function getProducts(req, res){
     const title = req.query.title;
     const genre = req.query.genre;
@@ -17,12 +19,12 @@ export async function getProducts(req, res){
             res.status(200).send(product);
         }
         if(genre){
-            const products = await db.collection('products').findAll({genre}).toArray();
+            const products = await db.collection('products').find({genre}).toArray();
 
             res.status(200).send(products);
         }
     }
-    catch(err){
+    catch(e){
         console.log(chalk.red.bold(`\nWARNING: getting products failed! \nError: \n`), e);
         res.status(500).send(e);
     }
